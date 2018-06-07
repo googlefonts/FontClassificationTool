60668f76974372dc1400f761fb94bb8d6efae82f
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2018-06-06 23:47:23 -0300

    updating the previous webtool

--- font-classification-tool.py
+++ font-classification-tool.py
@@ -38,6 +38,7 @@ import errno
 from fonts_public_pb2 import FamilyProto
 from constants import (NAMEID_FONT_FAMILY_NAME,
                        NAMEID_FONT_SUBFAMILY_NAME)
+from gfn import GFN_from_filename
 
 DESCRIPTION = """Calculates the visual weight, width or italic angle of fonts.
 
@@ -69,23 +70,6 @@ parser.add_argument("-o", "--output", default="output.csv", required=True,
 #TODO: make these available as CLI arguments as well:
 VERBOSE=True
 
-try:
-  from PIL import (Image,
-                   ImageDraw,
-                   ImageFont)
-except:
-  sys.exit("Needs pillow.\n\npip install pillow")
-
-
-try:
-  from fontTools.ttLib import TTFont
-except:
-  sys.exit("Needs fontTools.\n\npip install fonttools")
-
-try:
-  from google.protobuf import text_format
-except:
-  sys.exit("Needs protobuf.\n\npip install protobuf")
 
 try:
   from flask import (Flask,
@@ -96,35 +80,16 @@ try:
 except:
   sys.exit("Needs flask.\n\npip install flask")
 
-# The font size used to test for weight and width.
-FONT_SIZE = 30
 
-# The text used to test weight and width. Note that this could be
-# problematic if a given font doesn't have latin support.
-TEXT = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvXxYyZz"
+try:
+  from PIL import (Image,
+                   ImageDraw,
+                   ImageFont)
+except:
+  sys.exit("Needs pillow.\n\npip3 install pillow")
 
-# This text-block results in a better calculation of "darkness":
-TEXT_MULTILINE = ("AaBbCcDdEeAaBbCcDdEe\n"
-                  "FfGgHhIiJjFfGgHhIiJj\n"
-                  "KkLlMmNnOoKkLlMmNnOo\n"
-                  "PpQqRrSsTtPpQqRrSsTt\n"
-                  "UuVvXxYyZzUuVvXxYyZz")
 
-# Fonts that cause problems: any filenames containing these letters
-# will be skipped.
-# TODO: Investigate why these don't work.
-BLACKLIST = [
-#IOError: execution context too long (issue #703)
-  "Padauk",
-  "KumarOne",
-#ZeroDivisionError: float division by zero
-  "AdobeBlank",
-  "Phetsarath",
-# IOError: invalid reference See also: https://github.com/google/fonts/issues/132#issuecomment-244796023
-  "Corben",
-# IOError: stack overflow on text_width, text_height = font.getsize(TEXT) 
-  "Rubik",
-]
+
 
 def generate_italic_angle_images():
   for i in range(10):
@@ -144,13 +109,6 @@ def generate_italic_angle_images():
        os.mkdir(imagesdir)
     filepath = os.path.join(imagesdir, "angle_{}.png".format(i+1))
     im.save(filepath, "PNG")
-
-
-def get_FamilyProto_Message(path):
-    message = FamilyProto()
-    text_data = open(path, "rb").read()
-    text_format.Merge(text_data, message)
-    return message
 
 
 def normalize_values(properties, target_max=1.0):
@@ -191,211 +149,6 @@ ITALIC_ANGLE_TEMPLATE = """
      style="background:url(font_classification_tool/images/angle_%d.png) 0 0 no-repeat;" />
 """
 
-# The canonical [to Google Fonts] name comes before any aliases
-_KNOWN_WEIGHTS = collections.OrderedDict([
-    ('Thin', 100),
-    ('Hairline', 100),
-    ('ExtraLight', 200),
-    ('Light', 300),
-    ('Regular', 400),
-    ('', 400),  # Family-Italic resolves to this
-    ('Medium', 500),
-    ('SemiBold', 600),
-    ('Bold', 700),
-    ('ExtraBold', 800),
-    ('Black', 900)
-])
-
-FileFamilyStyleWeightTuple = collections.namedtuple(
-    'FileFamilyStyleWeightTuple', ['file', 'family', 'style', 'weight'])
-
-
-def StyleWeight(styleweight):
-  """Breaks apart a style/weight specifier into a 2-tuple of (style, weight).
-
-  Args:
-    styleweight: style/weight string, e.g. Bold, Regular, or ExtraLightItalic.
-  Returns:
-    2-tuple of style (normal or italic) and weight.
-  """
-  if styleweight.endswith('Italic'):
-    return ('italic', _KNOWN_WEIGHTS[styleweight[:-6]])
-
-  return ('normal', _KNOWN_WEIGHTS[styleweight])
-
-
-def FamilyName(fontname):
-  """Attempts to build family name from font name.
-
-  For example, HPSimplifiedSans => HP Simplified Sans.
-
-  Args:
-    fontname: The name of a font.
-  Returns:
-    The name of the family that should be in this font.
-  """
-  # SomethingUpper => Something Upper
-  fontname = re.sub('(.)([A-Z][a-z]+)', r'\1 \2', fontname)
-  # Font3 => Font 3
-  fontname = re.sub('([a-z])([0-9]+)', r'\1 \2', fontname)
-  # lookHere => look Here
-  return re.sub('([a-z0-9])([A-Z])', r'\1 \2', fontname)
-
-
-class ParseError(Exception):
-  """Exception used when parse failed."""
-
-
-def FileFamilyStyleWeight(filename):
-  """Extracts family, style, and weight from Google Fonts standard filename.
-
-  Args:
-    filename: Font filename, eg Lobster-Regular.ttf.
-  Returns:
-    FileFamilyStyleWeightTuple for file.
-  Raises:
-    ParseError: if file can't be parsed.
-  """
-
-  m = re.search(r'([^/-]+)-(\w+)\.ttf$', filename) #FAMILY_WEIGHT_REGEX
-  if not m:
-    raise ParseError('Could not parse %s' % filename)
-
-  sw = StyleWeight(m.group(2))
-  return FileFamilyStyleWeightTuple(filename,
-                                    FamilyName(m.group(1)),
-                                    sw[0],
-                                    sw[1])
-
-
-def _FileFamilyStyleWeights(fontdir):
-  """Extracts file, family, style, weight 4-tuples for each font in dir.
-
-  Args:
-    fontdir: Directory that supposedly contains font files for a family.
-  Returns:
-    List of FileFamilyStyleWeightTuple ordered by weight, style
-    (normal first).
-  Raises:
-    OSError: If the font directory doesn't exist (errno.ENOTDIR) or has no font
-    files (errno.ENOENT) in it.
-    RuntimeError: If the font directory appears to contain files from multiple
-    families.
-  """
-  if not os.path.isdir(fontdir):
-    raise OSError(errno.ENOTDIR, 'No such directory', fontdir)
-
-  files = glob.glob(os.path.join(fontdir, '*.ttf'))
-  if not files:
-    raise OSError(errno.ENOENT, 'no font files found')
-
-  result = [FileFamilyStyleWeight(f) for f in files]
-  def _Cmp(r1, r2):
-    return cmp(r1.weight, r2.weight) or -cmp(r1.style, r2.style)
-  result = sorted(result, _Cmp)
-
-  family_names = {i.family for i in result}
-  if len(family_names) > 1:
-    raise RuntimeError('Ambiguous family name; possibilities: %s'
-                       % family_names)
-
-  return result
-
-
-def get_gfn(fontfile, ttfont):
-  gfn = "unknown"
-  fontdir = os.path.dirname(fontfile)
-  metadata = os.path.join(fontdir, "METADATA.pb")
-  if os.path.exists(metadata):
-    family = get_FamilyProto_Message(metadata)
-    for font in family.fonts:
-      if font.filename in fontfile:
-        gfn = "{}:{}:{}".format(family.name, font.style, font.weight)
-        break
-  else:
-    try:
-      attributes = _FileFamilyStyleWeights(fontdir)
-      for (fontfname, family, style, weight) in attributes:
-        if fontfname in fontfile:
-          gfn = "{}:{}:{}".format(family, style, weight)
-          break
-    except:
-      pass
-
-  if gfn == 'unknown':
-    #This font lacks a METADATA.pb file and also failed
-    # to auto-detect the GFN value. As a last resort
-    # we'll try to extract the info from the NAME table entries.
-    try:
-      for entry in ttfont['name'].names:
-        if entry.nameID == NAMEID_FONT_FAMILY_NAME:
-          family = entry.string.decode(entry.getEncoding()).encode('ascii', 'ignore').strip()
-        if entry.nameID == NAMEID_FONT_SUBFAMILY_NAME:
-          style, weight = StyleWeight(entry.string.decode(entry.getEncoding()).encode('ascii', 'ignore').strip())
-      ttfont.close()
-      if family != "": #avoid empty string in cases of misbehaved family names in the name table
-        gfn = "{}:{}:{}".format(family, style, weight)
-        if VERBOSE:
-          print ("Detected GFN from name table entries: '{}' (file='{}')".format(gfn, fontfile))
-    except:
-      print("This seems to be a really bad font file...")
-      pass
-
-  if gfn == 'unknown':
-    print ("Failed to detect GFN value for '{}'. Defaults to 'unknown'.".format(fontfile))
-
-  return gfn
-
-
-fontinfo = {}
-blacklisted = []
-bad_darkness = []
-def analyse_fonts(files):
-  """Returns fontinfo dict"""
-  global blacklisted, bad_darkness, fontinfo
-
-  # run the analysis for each file, in sorted order
-  for count, fontfile in enumerate(sorted(files)):
-    # if blacklisted the skip it
-    if is_blacklisted(fontfile):
-      blacklisted.append(fontfile)
-      print >> sys.stderr, "[{}/{}] {} BLACKLISTED!".format(count+1, len(files), fontfile)
-      continue
-    else:
-      print("[{}/{}] {}...".format(count+1, len(files), fontfile))
-    # put metadata in dictionary
-    ttfont = TTFont(fontfile)
-    gfn = get_gfn(fontfile, ttfont)
-    if gfn not in fontinfo:
-      ttfont.close()
-      continue
-
-    darkness, width, img_d = get_darkness_and_width(fontfile)
-    img_d = render_single_line(fontfile)
-#    angle = get_angle(ttfont)
-    ttfont.close()
-
-    if darkness == 0:
-      bad_darkness.append(gfn)
-      if gfn in fontinfo:
-        darkness = fontinfo[gfn]['weight']
-      else:
-        continue
-
-    fontinfo[gfn]["weight"] = darkness
-    fontinfo[gfn]["img_weight"] = img_d
-    fontinfo[gfn]["fontfile"] = fontfile
-#   fontinfo[gfn]["width"] = width
-#   fontinfo[gfn]["angle"] = angle
-
-
-def is_blacklisted(filename):
-  """Returns whether a font is on the blacklist."""
-
-  # first check for explicit blacklisting:
-  for name in BLACKLIST:
-    if name in filename:
-      return True
 
 
 def get_angle(ttfont):
@@ -403,35 +156,11 @@ def get_angle(ttfont):
   return ttfont['post'].italicAngle
 
 
-def get_darkness_and_width(fontfile):
-  """Returns the darkness and width if a given a TTF.
-     Width is in pixels so it should be normalized."""
 
-  # Render the test text using the font onto an image.
-  font = ImageFont.truetype(fontfile, FONT_SIZE)
-  text_width, text_height = font.getsize(TEXT_MULTILINE)
-
-  # This is a trick to get a full block of text avoiding the
-  # beggining and end and sides of the actual rendered string
-  # bounding-box, because PIL seems to be bugged and sometimes
-  # adds some spurious white-space which would introduce
-  # errors in our calculation of the font darkness value:
-  img = Image.new('RGBA', (text_width/10, 5*text_height))
-  draw = ImageDraw.Draw(img)
-  draw.text((-text_width/20, -2.5*text_height),
-            TEXT_MULTILINE + '\n' + TEXT_MULTILINE,
-            font=font, fill=(0, 0, 0))
-
-  # Calculate the average darkness.
-  histogram = img.histogram()
-  avg = 0.0
-  for i in range(256):
-    alpha = histogram[i + 3*256]
-    avg += (i / 255.0) * alpha
-
-  darkness = avg / (text_width * text_height)
-  return darkness, text_width, get_base64_image(img)
-
+FONT_SIZE=30
+# The text used to test weight and width. Note that this could be
+# problematic if a given font doesn't have latin support.
+TEXT = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvXxYyZz"
 
 def render_single_line(fontfile):
   # Render the test text using the font onto an image.
@@ -453,27 +182,6 @@ def get_base64_image(img):
   return base64img
 
 
-def render_slant_chars(fontfile):
-  """Renders a sample of a few glyphs and
-     returns a PNG image as base64 data"""
-  # Disable this to speedup the tool
-  # We are currently not using this image
-  return ""
-
-  SAMPLE_CHARS = "HNHNUHNHN"
-  font = ImageFont.truetype(fontfile, FONT_SIZE * 10)
-  try:
-    text_width, text_height = font.getsize(SAMPLE_CHARS)
-  except:
-    text_width, text_height = 1, 1
-  img = Image.new('RGBA', (text_width, 20+text_height))
-  draw = ImageDraw.Draw(img)
-  try:
-    draw.text((0, 0), SAMPLE_CHARS, font=font, fill=(0, 0, 0))
-  except:
-    pass
-  return get_base64_image(img)
-
 
 def main():
   args = parser.parse_args()
@@ -489,28 +197,9 @@ def main():
   if len(files_to_process) == 0:
     sys.exit("No font files were found!")
 
-  if args.missingmetadata:
-    if args.existing == False:
-      sys.exit("you must use the --existing attribute in conjunction with --missingmetadata")
-    else:
-      rejected = []
-      with open(args.existing) as csvfile:
-        existing_data = csv.reader(csvfile, delimiter=',', quotechar='"')
-        next(existing_data) # skip first row as its not data
-        for row in existing_data:
-          name = row[0].split(':')[0]
-          if ' ' in name:
-            name = ''.join(name.split(' '))
-          for f, fname in enumerate(files_to_process):
-            if name in fname:
-              files_to_process.pop(f)
-              rejected.append(fname + ":" + row[0])
-
-      print("These files were removed from the list:\n" + '\n'.join(rejected))
-
-
+  fontinfo = {}
   # start with the existing values:
-  if args.existing and args.missingmetadata == False:
+  if args.existing:
     with open(args.existing) as csvfile:
         existing_data = csv.reader(csvfile, delimiter=',', quotechar='"')
         next(existing_data) # skip first row as its not data
@@ -521,41 +210,24 @@ def main():
             "angle_int": int(row[2]),
             "width_int": int(row[3]),
             "usage": row[4],
-            "gfn": gfn
+            "gfn": gfn,
+            "img_weight": None
           }
- 
-  # analyse fonts
-  analyse_fonts(files_to_process)
+
+  for fname in files_to_process:
+    gfn = GFN_from_filename(fname)
+    if gfn in fontinfo.keys():
+      fontinfo[gfn]['img_weight'] = render_single_line(fname)
+      # TODO: fontinfo[gfn]["weight"]
+      # TODO: "width" = width
+      # TODO: "angle" = angle
+
+  # analyse_fonts(files_to_process)
 
   if fontinfo == {}:
     sys.exit("All specified fonts are blacklisted!")
 
 
-  # normalise weights
-  weights = []
-  for key in sorted(fontinfo.keys()):
-    if "weight" not in fontinfo[key]:
-      del fontinfo[key]
-    else:
-      weights.append(fontinfo[key]["weight"])
-
-  ints = map_to_int_range(weights)
-  #print("weights: {}".format(weights))
-  #print("ints: {}".format(ints))
-  for count, key in enumerate(sorted(fontinfo.keys())):
-    fontinfo[key]['weight_int'] = ints[count]
-
-
-  # if we are debugging, just print the stuff
-  if args.debug:
-    items = ["weight", "weight_int", "width", "width_int",
-             "angle", "angle_int", "usage", "gfn"]
-    for key in sorted(fontinfo.keys()):
-       print fontinfo[key]["fontfile"],
-       for item in items:
-         print fontinfo[key][item],
-       print ""
-    sys.exit(0)
 
   # generate data for the web server
   # double(<unit>, <precision>, <decimal_point>, <thousands_separator>, <show_unit_before_number>, <nansymbol>)
@@ -581,16 +253,11 @@ def main():
   field_id = 1
   for key in fontinfo:
     values = fontinfo[key]
-#    if values["gfn"] == "unknown":
-#      continue
     img_weight_html = ""
     if values["img_weight"] is not None:
       img_weight_html = "<img height='50%%' src='data:image/png;base64,%s' />" % (values["img_weight"])
 
-    img_angle_html = ITALIC_ANGLE_TEMPLATE % (render_slant_chars(values["fontfile"]), values["angle_int"])
-
     values["image"] = img_weight_html
-    values["angle_image"] = img_angle_html
     grid_data["data"].append({"id": field_id, "values": values})
     field_id += 1
 
@@ -629,13 +296,13 @@ def main():
         row['values'][colname] = newvalue
     return save_csv()
 
-  if blacklisted:
-    print ("{} blacklisted font files:\n".format(len(blacklisted)))
-    print ("".join(map("* {}\n".format, blacklisted)))
+#  if blacklisted:
+#    print ("{} blacklisted font files:\n".format(len(blacklisted)))
+#    print ("".join(map("* {}\n".format, blacklisted)))
 
-  if bad_darkness:
-    print ("Failed to detect weight(darkness value) for {} of the font files:\n".format(len(bad_darkness)))
-    print ("".join(map("* {}\n".format, bad_darkness)))
+#  if bad_darkness:
+#    print ("Failed to detect weight(darkness value) for {} of the font files:\n".format(len(bad_darkness)))
+#    print ("".join(map("* {}\n".format, bad_darkness)))
 
   print ("\n\nAccess http://127.0.0.1:5000/font_classification_tool/index.html\n")
   app.run()
